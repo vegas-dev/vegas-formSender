8b87dcbe08707c8d97be305ae6fc1ea1a517da17
Alexander Vetlugin <vetlugins@gmail.com>
2024-08-03 15:09:40 +0300

    up

--- app/plugins/modal/VGModal.js
+++ app/plugins/modal/VGModal.js
@@ -8,60 +8,87 @@ class VGModal {
 			container: 'vg-form-sender--alert-modal',
 			backdrop:  'vg-form-sender--alert-backdrop'
 		}
+		this.element = null;
+		this.backdrop = null;
 	}
 
 	init() {
 		const _this = this;
 
-		let el = _this.form.querySelector('.' + _this.classes.container);
-		if (!el) el = _this.draw();
+		_this.element = _this.form.querySelector('.' + _this.classes.container);
+		if (!_this.element) _this.element = _this.draw();
 
-		_this.toggle(el);
+		_this.backdrop = document.createElement('div');
+		_this.backdrop.classList.add(_this.classes.backdrop);
+
+		_this.toggle();
 	}
 
-	toggle(el) {
-		let _this = this;
-
-		let backdrop = document.createElement('div');
-		backdrop.classList.add(_this.classes.backdrop);
-
+	toggle() {
 		if (!document.body.classList.contains('vg-modal-open')) {
-			document.body.classList.add('vg-modal-open');
-			el.style.display = 'block';
-
-			setTimeout(() => {
-				el.classList.add('active');
-				backdrop.classList.add('active');
-			}, 100);
-
-			document.body.append(backdrop);
+			return this.show();
+		} else {
+			return this.hide();
 		}
+	}
+
+	show() {
+		const _this = this;
+
+		document.body.classList.add('vg-modal-open');
+		_this.element.style.display = 'block';
+
+		setTimeout(() => {
+			_this.element.classList.add('active');
+			_this.backdrop.classList.add('active');
+		}, 100);
+
+		document.body.append(_this.element);
+		document.body.append(_this.backdrop);
+	}
+
+	hide() {
+		const _this = this;
+
+		document.body.classList.remove('vg-modal-open');
+		_this.element.classList.remove('active');
+		_this.backdrop.classList.remove('active')
+
+		setTimeout(() => {
+			_this.element.style.display = '';
+			_this.backdrop.remove();
+			_this.element.remove();
+		}, 100);
 	}
 
 	draw() {
 		const _this = this;
 
-		let alertModal = document.querySelector('.' + _this.classes.container);
-		if (alertModal) {
-			return alertModal;
-		}
-
 		let modal = document.createElement('div');
 		modal.classList.add(_this.classes.container);
 		modal.innerHTML = '<div class="modal-content"></div>';
-
-		document.body.append(modal);
 
 		return modal;
 	}
 }
 
-/*window.addEventListener('keydown', (event) => {
-	if (event.key === 'Escape' || event.key === 'Esc') {
-		if (document.body.classList.contains('vg-modal-open')) {
-			VGModal.close();
-		}
-	}
-});*/
-
 export default VGModal;
+
+/*
+'   <div class="modal-content-body">' +
+'       <div class="close"><a href="#" data-dismiss="alert-modal">' + getSvg('cross') + '</a></div>' +
+'       <div class="vg-alert vg-alert-danger">' +
+'	    <div class="svg-area">' + getSvg('error') + '</div>' +
+'	    <div class="content-area">' +
+'		    <div class="title-area" data-alert-danger-title></div>' +
+'		    <div class="text-area" data-alert-danger-text></div>' +
+'	    </div>' +
+'   </div>' +
+'   <div class="vg-alert vg-alert-success">' +
+'	    <div class="svg-area">' + getSvg('success') + '</div>' +
+'	    <div class="content-area">' +
+'		    <div class="title-area" data-alert-success-title></div>' +
+'		    <div class="text-area" data-alert-success-text></div>' +
+'	    </div>' +
+'   </div>' +
+'   </div>' +*/
